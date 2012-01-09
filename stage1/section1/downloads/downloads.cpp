38d170e9e5fc83403a9bcab33d24f1fe9a77461f
Gregory Liras <gregliras@gmail.com>
2012-01-10 00:47:40 +0200

    downloads complete

---
+++ stage1/section1/downloads/downloads.cpp
@@ -0,0 +1,45 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : downloads.cpp
+
+* Purpose :
+
+* Creation Date : 10-01-2012
+
+* Last Modified : Tue 10 Jan 2012 12:42:16 AM EET
+
+* Created By : Greg Liras <gregliras@gmail.com>
+
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <ostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    ifstream myinputfile("downloads.in");
+    ofstream myoutfile("downloads.out");
+    int n,bitrate,sec,bandwidth,totalsize;
+    float ans;
+    bandwidth=0;
+    totalsize=0;
+    myinputfile >> n;
+    for(int i=0;i<n;i++)
+    {
+        myinputfile >> bitrate >> sec;
+        bandwidth+=bitrate;
+        totalsize+=bitrate*sec;
+    }
+    ans = float(totalsize)/float(bandwidth);
+    if(ceil(ans)-ans>ans-floor(ans))
+        myoutfile << floor(ans) << endl;
+    else
+        myoutfile << ceil(ans) << endl;
+
+
+    return 0;
+}
