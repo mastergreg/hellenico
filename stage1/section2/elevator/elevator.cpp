cd4fd6f67c96bdac779b4c5bbdb88da7b945a6b5
Gregory Liras <gregliras@gmail.com>
2012-01-12 16:17:18 +0200

    elevator complete

---
+++ stage1/section2/elevator/elevator.cpp
@@ -0,0 +1,66 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : elevator.cpp
+
+* Purpose :
+
+* Creation Date : 12-01-2012
+
+* Last Modified : Thu 12 Jan 2012 04:16:21 PM EET
+
+* Created By : Greg Liras <gregliras@gmail.com>
+
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ifstream myinputfile("elevator.in");
+    ofstream myoutfile("elevator.out");
+
+    int n;
+    vector<int> input;
+    int times;
+    int big_m;
+    int intbuf;
+
+
+    myinputfile >> n;
+    if(n % 2 == 1)
+    {
+        input.reserve(n+1);
+        input.push_back(0);
+    }
+    for(int i=0;i<n;i++)
+    {
+        myinputfile >> intbuf;
+        input.push_back(intbuf);
+    }
+    sort(input.begin(),input.end());
+    times = input.size()/2;
+    n = input.size();
+    for(int i=0;i<times;i++)
+    {
+        input[i]+=input[n-i-1];
+    }
+    big_m=*max_element(input.begin(),input.begin()+times);
+
+    cout << times << " " << big_m << endl;
+    myoutfile << times << " " << big_m << endl;
+
+
+
+
+
+    myinputfile.close();
+    myoutfile.close();
+    return 0;
+}
+
+
